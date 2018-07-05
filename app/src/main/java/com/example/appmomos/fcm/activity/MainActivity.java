1c77fd31c9e12ff5c66afe35a9592f55dec84e65
vj <prachi@gmail.com>
2018-07-05 11:24:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/appmomos/fcm/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.appmomos.fcm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.appmomos.fcm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
